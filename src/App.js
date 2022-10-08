77b4113af9e4116f9af6e9a0a60590053c874f10
ChunWeiYeah <yher25@gmail.com>
2022-10-09 01:33:07 +0800

    更新程式碼

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Accounts from "./pages/Accounts";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
+
+const Permission = {
+  User: "User",
+  Admin: "Admin",
+};
 
 function App() {
+  const user = { login: true, permission: ["Admin"] };
   return (
     <div className="App">
       <Router>
@@ -14,8 +21,25 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/account" element={<Accounts />} />
+
+          <Route
+            element={
+              <ProtectedRoutes user={user} Permission={[Permission.Admin]} />
+            }
+          >
+            <Route path="/account" element={<Accounts />} />
+          </Route>
+
+          <Route
+            element={
+              <ProtectedRoutes
+                user={user}
+                Permission={[Permission.Admin, Permission.User]}
+              />
+            }
+          >
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
       </Router>
     </div>
--- src/utils/ProtectedRoutes.js
+++ src/utils/ProtectedRoutes.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoutes = ({ user, Permission }) => {
-  console.log(user);
-  return user.permission.find((per) => Permission.includes(per)) ? (
+  return user.login &&
+    user.permission.find((per) => Permission.includes(per)) ? (
     <Outlet />
   ) : (
     <Navigate to="/" />
